d57688b4d9be16d76573436233452cc3e3a97641
Alvin <thisiseasytoremember1224@gmail.com>
2024-01-08 09:58:02 -0600

    Initial Commit

---
+++ myWeatherAppNotesChen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWeatherAppNotesChen
+//
+//  Created by ALVIN CHEN on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
